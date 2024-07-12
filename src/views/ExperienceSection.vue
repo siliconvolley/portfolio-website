60a0a72a92e5110a941b5a423675e7cdaa583554
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-07-13 01:29:10 +0530

    added experience section

---
+++ src/views/ExperienceSection.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section id="experience" class="px-[20rem] my-[16rem]">
+    <p class="text-[4rem] font-semibold underline">Experience</p>
+    <div class="my-[8rem] grid gap-[2rem] px-[2rem]">
+      <div
+        class="h-[20rem] bg-dark-300 rounded-lg px-[4rem] flex place-items-center relative overflow-hidden"
+      >
+        <div>
+          <img
+            src="../assets/rocket.svg"
+            alt=""
+            class="absolute opacity-20 w-[50%] blur-md top-[-8rem] right-0"
+          />
+          <a href="" class="hover:underline text-[1.75rem] font-semibold"
+            >Title</a
+          >
+          <p class="text-[1.25rem] text-light-300 font-light my-[0.5em]">
+            Date
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
