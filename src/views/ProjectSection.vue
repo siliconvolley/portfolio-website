fa5db3563f837c0d8e9c0037545257c2aeb65036
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-07-13 01:29:35 +0530

    added project section

---
+++ src/views/ProjectSection.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import projects from "../data/projects.json";
+
+let baseImagePath: string = '../assets/project-images/'
+
+export interface RootInterface {
+  projects: Project[];
+}
+
+export interface Project {
+  title: string;
+  description: string;
+  tech_stack: string[];
+  card_image: string;
+  site_link: string;
+  repo_link: string;
+}
+
+</script>
+
+<template>
+  <section id="projects" class="px-[20rem] my-[16rem]">
+    <p class="text-[4rem] font-semibold underline">Projects</p>
+    <div class="my-[8rem] grid grid-cols-3 gap-[4rem] px-[2rem]">
+      <div v-for="project in projects">
+        <div class="project-card">
+          <a
+            :href="project.site_link"
+            rel="noopener noreferrer"
+            target="_blank"
+            class="h-full grid place-items-center"
+          >
+          <!-- ! Fix the image path not displaying the image -->
+            <img
+              :src="baseImagePath.concat(project.card_image)"
+              alt="Project Sample Image"
+              class=""
+            />
+          </a>
+        </div>
+        <div>
+          <p class="text-[1.75rem] font-semibold my-[0.25em]">
+            <a
+              :href="project.repo_link"
+              rel="noopener noreferrer"
+              target="_blank"
+              class="hover:underline"
+              >{{ project.title }}</a
+            >
+          </p>
+          <p class="text-[1.2rem] font-light text-light-300 my-[0.5em]">
+            {{ project.description }}
+          </p>
+          <ul class="font-medium flex gap-2 text-center">
+            <li v-for="tech in project.tech_stack" class="bg-dark-100 min-w-[4rem] p-2 rounded-md">{{ tech }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+.project-card {
+  background: var(--clr-dark-300);
+  height: 16rem;
+  display: grid;
+  place-items: center;
+  border-radius: 0.5rem;
+  overflow: hidden;
+}
+</style>
