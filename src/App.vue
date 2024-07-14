7846d6fba03edcab5ced5d81153e2e228d776e78
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-07-14 15:36:45 +0530

    added scroll to top button

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import Footer from "./components/Footer.vue";
 import Navbar from "./components/Navbar.vue";
 import MainSection from "./views/MainSection.vue";
+import Footer from "./components/Footer.vue";
+import ScrollTopButton from "./components/ScrollTopButton.vue";
 </script>
 
 <template>
   <Navbar />
-  <MainSection />  
+  
+  <MainSection />
+  <ScrollTopButton />
+  
   <Footer />
 </template>
