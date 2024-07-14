bf57be8e643ddf07b94834a1f7bf8649cd581999
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-07-14 15:41:10 +0530

    increased the footer height abit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="h-[4rem] grid place-items-center font-medium">
+  <div class="h-[8rem] grid place-items-center font-medium">
     <span class="grid grid-flow-col place-items-center gap-2 text-[1.25rem]">nothing sus over here ;) </span>
   </div>
 </template>
