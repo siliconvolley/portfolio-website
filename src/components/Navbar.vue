52aa73766098f83a7ad9ff6fd6f2b8637c3b799a
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-07-21 20:09:04 +0530

    made it responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
+
+const isMenuOpen = ref(false);
 
 onMounted(() => {
   const navigationBar = document.getElementById("navigationBar");
@@ -11,20 +13,36 @@ onMounted(() => {
     );
   }
 });
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
 </script>
 
 <template>
   <nav
     id="navigationBar"
-    class="flex justify-between h-[6rem] sticky top-0 backdrop-blur-[8px] text-[1.5rem] p-[2rem] z-[1000]"
+    class="w-full h-auto text-[1.5rem] sticky top-0 flex justify-between items-center backdrop-blur-[8px] z-[1000] p-8"
   >
-    <a href="/">Rishabh Laxmikanth</a>
-    <ul class="flex gap-x-[4em]">
-      <li><a href="#projects" class="hover:underline">Projects</a></li>
-      <li><a href="#experience" class="hover:underline">Experience</a></li>
-      <li>
+    <a href="/" class="w-full">Rishabh Laxmikanth</a>
+    <ul
+      v-bind:class="{ open: isMenuOpen }"
+      class="menu bg-dark-400 w-full grid place-items-center absolute top-[100%] left-0 right-0 overflow-hidden md:bg-inherit md:flex md:justify-end md:gap-x-[4em] md:relative md:overflow-visible"
+    >
+      <li class="my-4 md:my-0">
+        <a href="#projects" @click="toggleMenu" class="hover:underline"
+          >Projects</a
+        >
+      </li>
+      <li class="my-4 md:my-0">
+        <a href="#experience" @click="toggleMenu" class="hover:underline"
+          >Experience</a
+        >
+      </li>
+      <li class="my-4 md:my-0">
         <a
           href="https://drive.google.com/file/d/1lmjx7B_v8xFAHe2Rui8m6CFAYw5_wPU-/view?usp=sharing"
+          @click="toggleMenu"
           class="hover:underline"
           rel="noopener noreferrer"
           target="_blank"
@@ -32,5 +50,19 @@ onMounted(() => {
         >
       </li>
     </ul>
+    <button @click="toggleMenu" class="hover:underline focus:underline md:hidden">
+      Menu
+    </button>
   </nav>
 </template>
+
+<style scoped>
+.menu {
+  max-height: 0;
+  transition: max-height 0.3s ease-in-out;
+}
+
+.menu.open {
+  max-height: 500px;
+}
+</style>
